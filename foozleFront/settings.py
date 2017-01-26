b97037180a60d0823eca3b64d3bc0e4ea001e90b
jojo <jonathan.rodriguez@roiback.com>
2017-01-26 17:08:39 +0100

    fixing settings

--- foozleFront/settings.py
+++ foozleFront/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import dj_database_url
-from decouple import Csv, config
-from unipath import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,12 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'fqg4po)lp$j483zcppov)!zj(x$jmzr9bx(plt18oke4_3tlr1'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'fqg4po)lp$j483zcppov)!zj(x$jmzr9bx(plt18oke4_3tlr1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -58,8 +53,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'foozleFront.urls'
-print "=" * 90
-print os.path.join(BASE_DIR, 'templates/')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -100,10 +93,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-if dj_database_url.config():
-    DATABASES['default'] = dj_database_url.config()
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
