62a5cd27b4e09055e0968c28b5c306e35e95ede6
jojo <jonathan.rodriguez@roiback.com>
2017-01-26 15:52:32 +0100

    fix static root

--- foozleFront/settings.py
+++ foozleFront/settings.py
@@ -72,12 +72,13 @@ TEMPLATES = [
     },
 ]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "foozleFront/static/"),
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 WSGI_APPLICATION = 'foozleFront.wsgi.application'
 
 
@@ -128,9 +129,4 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/login/'
