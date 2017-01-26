a6a9b85f97e9170035e5472922bb16d2ecb0ce79
jojo <jonathan.rodriguez@roiback.com>
2017-01-26 16:02:57 +0100

    fix databases settings

--- foozleFront/settings.py
+++ foozleFront/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import Csv, config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,10 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fqg4po)lp$j483zcppov)!zj(x$jmzr9bx(plt18oke4_3tlr1'
+# SECRET_KEY = 'fqg4po)lp$j483zcppov)!zj(x$jmzr9bx(plt18oke4_3tlr1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
@@ -86,16 +90,10 @@ WSGI_APPLICATION = 'foozleFront.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'foozlejs',
-        'USER': 'foozleJS',
-        'PASSWORD': 'f00zl3!',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
